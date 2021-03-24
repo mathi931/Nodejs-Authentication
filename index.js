b73fa643824975d6afc4ebc3b96f6b7832ef68d9
unknown <mesi.mathi@gmail.com>
2021-03-24 10:40:16 +0100

    created user model

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+//connect to DB
+mongoose.connect(
+    process.env.DB_CONNECT,
+{ useUnifiedTopology: true },
+()=> console.log('connected to db!'));
 
 //Import Routes
 const authRoute=  require('./routes/auth');
