363803cc30aa928e0fa4f308ed9fae48c110459a
unknown <mesi.mathi@gmail.com>
2021-03-31 13:30:30 +0200

    password Hash, used: bcryptjs, and email check

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../model/User');
 const { registerValidation } = require('../validation');
+const bcrypt = require('bcryptjs');
 
 //Register new user
 router.post('/register', async (req, res) => {
@@ -8,15 +9,25 @@ router.post('/register', async (req, res) => {
 	const { error } = registerValidation(req.body);
 	if (error) return res.status(400).send(error.details[0].message);
 
+	//checking if the email aready exist
+	const emailExist = await User.findOne({ email: req.body.email });
+	if (emailExist) {
+		return res.status(400).send('Email already exists');
+	}
+
+	//Hash password
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
 	//create the new user after validation
 	const user = new User({
 		name: req.body.name,
 		email: req.body.email,
-		password: req.body.password,
+		password: hashedPassword,
 	});
 	try {
 		const savedUser = await user.save();
-		res.send(savedUser);
+		res.send({ user: user._id });
 	} catch (err) {
 		res.status(400).send(err);
 	}
