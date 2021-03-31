eb40c926cd0dc26782d2fb119ea31c2b76b61b91
unknown <mesi.mathi@gmail.com>
2021-03-31 14:27:36 +0200

    jsonWebToken

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const User = require('../model/User');
-const { registerValidation } = require('../validation');
+const jwt = require('jsonwebtoken');
+const { registerValidation, logInValidation } = require('../validation');
 const bcrypt = require('bcryptjs');
 
-//Register new user
+
+//REGISTER
 router.post('/register', async (req, res) => {
 	//validate the data before
 	const { error } = registerValidation(req.body);
@@ -27,14 +29,34 @@ router.post('/register', async (req, res) => {
 	});
 	try {
 		const savedUser = await user.save();
-		res.send({ user: user._id });
+		//res.send({ user: user._id });
+		res.send(savedUser);
 	} catch (err) {
 		res.status(400).send(err);
 	}
 });
 
-router.post('/login', (req, res) => {
-	res.send('sucess login!');
+//LOGIN
+router.post('/login', async (req, res) => {
+	//validate the data before
+	const { error } = logInValidation(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+
+	//checking if the email is registered already
+	//and the password is correct
+	const validUser = await User.findOne({ email: req.body.email });
+	const validPass = await bcrypt.compare(req.body.password, validUser.password);
+
+	if (!validUser || !validPass) {
+		return res.status(400).send('This match doesnt exists');
+	}
+
+	//if both are OK assign a token and send a respond
+    else if(validUser && validPass){
+        const token = jwt.sign({ _id: validUser._id }, process.env.TOKEN_SECRET);
+        res.header('auth-token', token).send(token);
+    }
+
 });
 
 //Get all users
