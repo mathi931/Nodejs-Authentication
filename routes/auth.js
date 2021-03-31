3b109e63473f50c5a0ba61d61b6b65b8dde7a52d
unknown <mesi.mathi@gmail.com>
2021-03-31 12:36:42 +0200

    validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
 const User = require('../model/User');
-
-//VALIDATION
-
+const { registerValidation } = require('../validation');
 
 //Register new user
 router.post('/register', async (req, res) => {
+	//validate the data before
+	const { error } = registerValidation(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+
+	//create the new user after validation
 	const user = new User({
 		name: req.body.name,
 		email: req.body.email,
